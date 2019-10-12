2508b6d35bfdf2afd8fa9c2434e0917d2a202606
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-12 21:29:33 +0100

    Rule of five implementation
    Signed-off-by: Ayoya22 <djoukamsimplice@gmail.com>

--- src/chatbot.h
+++ src/chatbot.h
@@ -1,7 +1,10 @@
 #ifndef CHATBOT_H_
 #define CHATBOT_H_
 
-#include <iostream>
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
 #include <string>
 
 class GraphNode; // forward declaration
@@ -10,26 +13,39 @@ class ChatLogic; // forward declaration
 class ChatBot
 {
 private:
+    // data handles (owned)
+    wxBitmap *picture; // avatar imagez
 
     // data handles (not owned)
+    ChatLogic *_chatLogic;
     GraphNode *_currentNode;
     GraphNode *_rootNode;
-    ChatLogic *_chatLogic;
 
     // proprietary functions
     int ComputeLevenshteinDistance(std::string s1, std::string s2);
 
 public:
     // constructors / destructors
-   ChatBot(){
-        std::cout << "ChatBot Constructor"<< std::endl;
-    }
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
     ~ChatBot();
+
+    //// STUDENT CODE
+    ////
+
+    ChatBot(const ChatBot &source);//copy constructor declaration
+    ChatBot( ChatBot &&source);//move constructor declaration
+    ChatBot &operator=(const ChatBot &source);//copy assignment operator declaration
+    ChatBot &operator=(ChatBot &&source);//move assignment operator declaration
+
+    ////
+    //// EOF STUDENT CODE
 
     // getters / setters
     void SetCurrentNode(GraphNode *node);
     void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
     void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    wxBitmap *GetImageHandle() { return picture; }
 
     // communication
     void ReceiveMessageFromUser(std::string message);
